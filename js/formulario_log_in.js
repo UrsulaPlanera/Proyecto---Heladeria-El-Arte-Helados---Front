747e02b58c60c36801fc12f7b32708abe49fa030
loliita51 <alexandraplanera@gmail.com>
2023-12-11 01:28:05 -0300

    add .

--- js/formulario_log_in.js
+++ js/formulario_log_in.js
@@ -6,7 +6,7 @@ let code = document.getElementById('input-code');
 function validar(){
     form.addEventListener("submit", e => {
         e.preventDefault()
-        if (usuario.value == 'admin' && contraseña.value == 'admin@elartehelados.admin.com' && code.value == 1213){
+        if (usuario.value == 'admin' && contraseña.value == 123 && code.value == 456){
             window.open('../gestor/inicio/gestor.html', '_blank')
         }
         else {
